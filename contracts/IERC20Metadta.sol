7c3ce8487b8348bc8f2cc10f57bce0db87613117
magistrigusti <47912728+magistrigusti@users.noreply.github.com>
2025-07-04 18:05:03 +0300

    0.1.5

--- contracts/IERC20Metadta.sol
+++ contracts/IERC20Metadta.sol
@@ -3,5 +3,9 @@ pragma solidity ^0.8.28;
 import "./IERC20.sol";
 
 interface IERC20Metadata is IERC20 {
-  function name
+  function name() external view returns(string memory);
+  
+  function symbol() external view returns(string memory);
+
+  function decimals() external view returns(uint8);
 }
