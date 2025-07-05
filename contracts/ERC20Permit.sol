c8f5e64b92188f9e4d7c0aae2ad495c46b84d70d
magistrigusti <47912728+magistrigusti@users.noreply.github.com>
2025-07-05 16:22:43 +0300

    0.2

---
+++ contracts/ERC20Permit.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.28;
+import "./IERC20Metadata.sol";
+import "./IERC20Permit.sol";
+import "./ERC20.sol";
+import "./utils/Counters.sol";
+import "./crypto/ECDSA.sol";
+import "./crypto/EIP712.sol";
+
---
+++ contracts/crypto/EIP712.sol
@@ -0,0 +1,5 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.28;
+import "./ECDSA.sol";
+import "../utils/ShortStrings.sol";
+import "../IERC5267.sol";
