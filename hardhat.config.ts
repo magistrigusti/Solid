8728ae601a00578c49595789414a534a3b6cce73
magistrigusti <47912728+magistrigusti@users.noreply.github.com>
2025-07-05 15:45:36 +0300

    0.1.8

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -2,7 +2,20 @@ import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.28",
+  solidity: {
+    version: "0.8.28",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200
+      },
+    },
+  },
+  networks: {
+    hardhat: {
+      chainId: 1337
+    },
+  },
 };
 
 export default config;
