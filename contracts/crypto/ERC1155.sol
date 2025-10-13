1cc9332ad205eb9098422a110c2872e07adbc6bb
magistrigusti <47912728+magistrigusti@users.noreply.github.com>
2025-10-13 07:29:15 +0300

    0.4.1

--- contracts/crypto/ERC1155.sol
+++ contracts/crypto/ERC1155.sol
@@ -1,8 +1,10 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.28;
 import "../interfaces/IERC1155MetadataURI.sol";
+import "../interfaces/IERC1155.sol";
+import "../interfaces/IERC1155Receiver.sol";
 
-contract ERC1155 is IERC1155MetadataURI {
+contract ERC1155 is IERC1155, IERC1155MetadataURI {
   mapping (uint => mapping(address => uint)) private _balances;
   mapping (address => mapping(address => bool)) private _operatorApprovals;
   string private _uri;
@@ -49,7 +51,7 @@ contract ERC1155 is IERC1155MetadataURI {
 
   function isApprovedForAll(
     address account, address operator
-  ) external view virtual returns(bool) {
+  ) public view virtual returns(bool) {
     return _operatorApprovals[account][operator];
   }
 
@@ -106,5 +108,119 @@ contract ERC1155 is IERC1155MetadataURI {
     _afterTokenTransfer(operator, from, to, ids, amounts, data);
 
     _doSafeTransferAcceptanceCheck(operator, from, to, id, amount, data);
+  }
+
+  function _safeBatchTransferFrom(
+    address from, 
+    address to,
+    uint[] calldata ids,
+    uint[] calldata amount,
+    bytes calldata data
+  ) internal virtual {
+    require(ids.length == amounts.length);
+    require(to != address(0));
+
+    address operator = msg.sender;
+
+    _beforeTokenTransfer(operator, from, to, ids, amounts, data);
+
+    for(uint i = 0; i < ids.length; ++i) {
+      uint id = ids[i];
+
+      uint amount = amounts[i];
+
+      uint fromBalance = _balances[id][from];
+      require(fromBalance >= amount);
+      _balances[id][from] = fromBalance - amount;
+      _balances[id][to] += amount;
+    }
+
+    emit TransferBatch(operator, from, to, ids, amounts);
+    _afterTokenTransfer(operator, from, to, ids, amounts, data);
+    _doSafeBatchTransferAcceptanceCheck(operator, from, to, ids, amounts, data);
+  }
+
+  function _setURI(string memory newUri) internal virtual {
+    _uri = newUri;
+  }
+
+  function _setApprovalForAll(
+    address owner,
+    address operator,
+    bool approved
+  ) internal virtual {
+    require(owner != operator);
+
+    _operatorApprovals[owner][operator] = approved;
+    emit ApprovalForAll(owner, operator, approved);
+  }
+
+  function _beforeTokenTransfer(
+    address operator,
+    address from,
+    address to,
+    uint[] memory ids,
+    uint[] memory amounts,
+    bytes memory data
+  ) internal virtual {}
+
+  function _afterTokenTransfer(
+    address operator,
+    address from,
+    address to,
+    uint[] memory ids,
+    uint[] memory amounts,
+    bytes memory data
+  ) internal virtual {}
+
+  function _doSafeTransferAcceptanceCheck(
+    address operator,
+    address from,
+    address to,
+    uint id,
+    uint amount,
+    bytes memory data
+  ) private {
+    if (to.code.length > 0) {
+      try IERC1155Receiver(to).onERC1155Received(
+        operator, from, id, amount, data
+      ) returns(bytes4 resp) {
+        if (resp != IERC1155Receiver.onERC1155Received.selector) {
+          revert("Rejected tokens!");
+        }
+      } catch Error(string memory reason) {
+        revert(reason);
+      } catch {
+        revert("Non-ERC1155 receiver!");
+      }
+    }
+  }
+
+  function _doSafeBatchTransferAcceptanceCheck(
+    address operator,
+    address from,
+    address to,
+    uint[] ids,
+    uint[] amounts,
+    bytes memory data
+  ) private {
+    if (to.code.length > 0) {
+      try IERC1155Receiver(to).onERC1155BatchReceived(
+        operator, from, ids, amounts, data
+      ) returns(bytes4 resp) {
+        if (resp != IERC1155Receiver.onERC1155BatchReceived.selector) {
+          revert("Rejected tokens!");
+        }
+      } catch Error(string memory reason) {
+        revert(reason);
+      } catch {
+        revert("Non-ERC115 receiver!");
+      }
+    }
+  }
+
+  function _asSingJettonArray(uint el) private pure returns(uint[] memory result) {
+    result = new uint[](1);
+    result[0] = el;
   }
 }
